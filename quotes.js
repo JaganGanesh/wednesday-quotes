595b6bb633d9160c5454ffc45fa64c3f343bf080
Jagan Ganesh <JaganGanesh@proton.me>
2023-05-21 12:01:43 +0530

    Initial Commit

---
+++ quotes.js
@@ -0,0 +1,164 @@
+'use strict';
+
+module.exports = [
+    {
+        "quote": "I'm not sure whose twisted idea it was to put hundreds of adolescents in underfunded schools run by people whose dreams were crushed years ago, but I admire the sadism",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Pugsley, emotion equals weakness",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Wednesday, what are you gonna go?",
+        "author": "Pugsley Pubert Addams"
+    },
+    {
+        "quote": "Nevermore is like no other boarding school. It's a magical place",
+        "author": "Gomez Addams"
+    },
+    {
+        "quote": "Wednesday always looks half dead.",
+        "author": "Gomez Addams"
+    },
+    {
+        "quote": "Please excuse Wednesday, she's allergic to color.",
+        "author": "Morticia Addams"
+    },
+    {
+        "quote": "I find social media to be a soul sucking void of meaningless affirmation.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Look at you my little deathtrap.",
+        "author": "Gomez Addams"
+    },
+    {
+        "quote": "You're a brilliant girl, Wednesday, but sometimes you get in your own way.",
+        "author": "Morticia Addams"
+    },
+    {
+        "quote": "Too odd for the normies, not odd enough for the outcasts.",
+        "author": "Ms. Thornhill"
+    },
+    {
+        "quote": "These are all traits of great writers. And serial killers.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Sometimes I act like I don't care if people like me. Deep down, I secretly enjoy it.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "The most interesting plants grow in the shade.",
+        "author": "Ms. Thornhill"
+    },
+    {
+        "quote": "I like to win. Is that so wrong?",
+        "author": "Bianca Barclay"
+    },
+    {
+        "quote": "Wednesday Addams is not the girl of your dreams. She's the stuff of your nightmares.",
+        "author": "Bianca Barclay"
+    },
+    {
+        "quote": "Being your friend should come with a warning label.",
+        "author": "Xavier Thorpe"
+    },
+    {
+        "quote": "It's amateurs like you who give kidnapping a bad game.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Why am I sharing this apocalypse with a pilgrim?",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Are you mansplaining my power?",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Emotion isn't exactly your strong suit.",
+        "author": "Tyler Galpin"
+    },
+    {
+        "quote": "I don't believe in heaven or hell, but I do believe in revenge.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Those who forget history are doomed to repeat it.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Enjoy your solitude, Wednesday.",
+        "author": "Enid Sinclair"
+    },
+    {
+        "quote": "I almost killed you.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Beginner's luck.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "There's nothing quite like the feeling of being proven right.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Because trust and cooperation have always been the hallmarks of our relationship.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "I can't believe I'm actually going to miss your creepy, lifeless eyes waking me up every morning.",
+        "author": "Enid Sinclair"
+    },
+    {
+        "quote": "Anytime I grow nauseous at the sight of a rainbow or hear a pop song that makes my ears bleed, I'll think of you.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "I've learned so much from you. Part of it is admittedly criminal behavior",
+        "author": "Enid Sinclair"
+    },
+    {
+        "quote": "Not hugging is kind of our thing.",
+        "author": "Enid Sinclair"
+    },
+    {
+        "quote": "When the dance floor calls, you gotta answer.",
+        "author": "Eugene Ottinger"
+    },
+    {
+        "quote": "Kind of a deja vu thing we got going on huh?",
+        "author": "Tyler Galpin"
+    },
+    {
+        "quote": "Except I won't cry and whine like a child.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "I can't believe you were in a secret society and didn't tell me.",
+        "author": "Enid Sinclair"
+    },
+    {
+        "quote": "You're like a cockroach.",
+        "author": "Tyler Galpin"
+    },
+    {
+        "quote": "Please. Flattery will get you nowhere.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Never meet your heroes",
+        "author": "Ms. Thornhill"
+    },
+    {
+        "quote": "You brought a gun to a sword fight. Probably the first smart decision you've made today.",
+        "author": "Wednesday Addams"
+    },
+    {
+        "quote": "Hummers stick together, right?",
+        "author": "Eugene Ottinger"
+    }
+];
